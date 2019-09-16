2fa46cfb57a0de097ed227731192cfcd0baf176c
ramdan0cool <32842840+ramdan0cool@users.noreply.github.com>
2019-09-16 18:28:44 +0700

    Initial upload

---
+++ Scripts/Car/Car.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    [SerializeField] private CarSettings carSetting;
+
+    private ICarInput carInput;
+    private CarMotor carMotor;
+    private CarPhysics carPhysic;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        carInput = new KeyboardInput();
+        carMotor = new CarMotor(carInput, rb = GetComponent<Rigidbody>(), carSetting);
+        carPhysic = new CarPhysics(carInput, rb);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        carInput.ReadInput();
+        carMotor.Move();
+        Debug.Log(rb.velocity.x);
+        //carPhysic.Friction();
+        //Friction();
+        
+    }
+
+    
+}
---
+++ Scripts/Car/ICarInput.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICarInput
+{
+    void ReadInput();
+    bool Throttle { get; }
+    bool Brake { get; }
+}
