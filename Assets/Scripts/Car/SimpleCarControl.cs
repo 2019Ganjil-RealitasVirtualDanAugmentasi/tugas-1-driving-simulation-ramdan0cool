048bb241d3c28c81c18d65cc124ceb6248de2b41
Ramdan <ramadani23@rocketmail.com>
2019-09-17 06:02:12 +0700

    Can drive

--- Assets/Scripts/Car/SimpleCarControl.cs
+++ Assets/Scripts/Car/SimpleCarControl.cs
@@ -2,24 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SimpleCarControl : MonoBehaviour, IRequireUserInput
+public class SimpleCarControl : MonoBehaviour
 {
     #region Variables
-    private Rigidbody rb;
-    private float currentSpeed, forward;
+    //private Rigidbody rb;
+    //private float currentSpeed, forward;
 
-    [SerializeField] private float maxSpeed;
-    [SerializeField] private float frictionValue;
-    [SerializeField] private float accelerationValue;
-    [SerializeField] private float brakeValue;
+    //[SerializeField] private float maxSpeed;
+    //[SerializeField] private float frictionValue;
+    //[SerializeField] private float accelerationValue;
+    //[SerializeField] private float brakeValue;
 
-    public IUserInput InputProxy { get; set; }
+    //public IUserInput InputProxy { get; set; }
+
+    public List<AxleInfo> axleInfos;
+    public float maxMotorTorque;
+    public float maxSteeringAngle;
+
     #endregion
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+    //    rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -46,15 +51,45 @@ public class SimpleCarControl : MonoBehaviour, IRequireUserInput
         //else if (currentSpeed != 0) currentSpeed += currentSpeed > 0 ? -frictionValue : frictionValue;
         ////if (currentSpeed < 0) currentSpeed = 0f;
         //rb.velocity = new Vector3(-currentSpeed, rb.velocity.y, rb.velocity.z);   
-        bool accelerate = Input.GetKey(KeyCode.W);
-        bool brake = Input.GetKey(KeyCode.S);
 
-        if (accelerate) currentSpeed += currentSpeed < (maxSpeed - accelerationValue) ? accelerationValue : 0;
-        else if (brake) currentSpeed -= currentSpeed > 0 ? brakeValue : 0f; 
-        rb.velocity = new Vector3(-currentSpeed, rb.velocity.y, rb.velocity.z);
 
-        if (currentSpeed >= frictionValue || currentSpeed < -frictionValue)
-            currentSpeed += currentSpeed > 0 ? -frictionValue : frictionValue;
-        else currentSpeed = 0;
+        //bool accelerate = Input.GetKey(KeyCode.W);
+        //bool brake = Input.GetKey(KeyCode.S);
+
+        //if (accelerate) currentSpeed += currentSpeed < (maxSpeed - accelerationValue) ? accelerationValue : 0;
+        //else if (brake) currentSpeed -= currentSpeed > 0 ? brakeValue : 0f; 
+        //rb.velocity = new Vector3(-currentSpeed, rb.velocity.y, rb.velocity.z);
+
+        //if (currentSpeed >= frictionValue || currentSpeed < -frictionValue)
+        //    currentSpeed += currentSpeed > 0 ? -frictionValue : frictionValue;
+        //else currentSpeed = 0;
     }
+
+    private void FixedUpdate()
+    {
+        float motor = maxMotorTorque * -Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach (AxleInfo info in axleInfos)
+        {
+            if(info.steering)
+            {
+                info.leftWheel.steerAngle = steering;
+                info.rightWheel.steerAngle = steering;
+            }    
+            if(info.motor)
+            {
+                info.leftWheel.motorTorque = motor;
+                info.rightWheel.motorTorque = motor;
+            }
+        }
+    }
+}
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor;
+    public bool steering;
 }
